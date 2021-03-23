d1a90795fd8e8bc8066e60aaebf949031e67b392
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-03-23 21:27:27 +0100

    Add context initializing

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,18 @@ public class App
 {
     public static void main( String[] args )
     {
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ctx.start();
+        System.out.println("Application context started");
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ctx.stop();
+        ctx.close();
+
         System.out.println( "Hello World!" );
     }
 }
