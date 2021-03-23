f441257d22dc3c01ac2c4a7434f6471ec19db73c
Bartosz Maleta <bartosz.maleta@gmail.com>
2021-03-23 21:27:59 +0100

    Add overrided configure() with inheriance of RouteBuilder

--- src/main/java/org/example/route/SimpleRouteBuilder.java
+++ src/main/java/org/example/route/SimpleRouteBuilder.java
@@ -1,4 +1,12 @@
 package org.example.route;
 
-public class SimpleRouteBuilder {
+import org.apache.camel.builder.RouteBuilder;
+import org.example.processor.MyProcessor;
+
+public class SimpleRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("file:/home/acer/PROGRAMMING/Jobs/PKO_BP_JJD/ApacheCamel/simple_Java_DSL_copying_files app/inputFolder?noop=true").process(new MyProcessor()).to("file:/home/acer/PROGRAMMING/Jobs/PKO_BP_JJD/ApacheCamel/simple_Java_DSL_copying_files_app/outputFolder");
+    }
 }
